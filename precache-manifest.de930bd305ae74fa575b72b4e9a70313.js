502f9b839001125621c1165c69f4845534af6d70
Jacob <perezjacob2@gmail.com>
2020-07-18 22:09:12 -0500

    Updates

---
+++ precache-manifest.de930bd305ae74fa575b72b4e9a70313.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "039cf6ce8595f3f49ecf96ed15e42880",
+    "url": "/sneakers/index.html"
+  },
+  {
+    "revision": "2d5c41797889952fd012",
+    "url": "/sneakers/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3906e43fe9f85c9159d5",
+    "url": "/sneakers/static/css/main.46f6a53c.chunk.css"
+  },
+  {
+    "revision": "2d5c41797889952fd012",
+    "url": "/sneakers/static/js/2.15d0398d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sneakers/static/js/2.15d0398d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3906e43fe9f85c9159d5",
+    "url": "/sneakers/static/js/main.1bd6caa3.chunk.js"
+  },
+  {
+    "revision": "5fe612d6832e4cf1222d",
+    "url": "/sneakers/static/js/runtime-main.da45dab3.js"
+  }
+]);
